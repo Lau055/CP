4b0c75b16945bd1cb5f5e53524139cf287824ec0
Lau055 <116377504+Lau055@users.noreply.github.com>
2023-01-14 23:08:48 +0000

    Add files via upload

--- TP2/src/k_means_with_1st_opt.c
+++ TP2/src/k_means_with_1st_opt.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<omp.h>
+#include <pthread.h>
+
 
 typedef struct Ponto {
     float x, y;
@@ -9,6 +11,7 @@ typedef struct Ponto {
 
 Ponto *cluster, *ponto;
 int K =0, N = 0, T =0;
+
 
 void inicializa() {
     srand(10);
@@ -26,58 +29,62 @@ void inicializa() {
 }
 
 void colocar() {
-
-    #pragma omp parallel for
-
+    float d;
+    float distancia;
     for (int i = 0; i < N/2; i++) {
-        float distancia = 1000000000;
+        distancia = 1000000000;
         for (int j = 0; j < K; j++) {
-            float d = (cluster[j].x - ponto[i].x) * (cluster[j].x - ponto[i].x) +
+            d = (cluster[j].x - ponto[i].x) * (cluster[j].x - ponto[i].x) +
                       (cluster[j].y - ponto[i].y) * (cluster[j].y - ponto[i].y);
             ponto[i].cluster = (d < distancia ? j : ponto[i].cluster);
             distancia = d < distancia ? d : distancia;
+            //printf("Analisys :T%d cluster (%f,%f), ponto (%f,%f)\n", id, cluster[j].x,
+            //cluster[j].y,ponto[i].x,ponto[i].y);
         }
     }
-    #pragma omp parallel for 
-
     for (int i = N/2; i < N; i++) {
-        float distancia = 1000000000;
+        distancia = 1000000000;
         for (int j = 0; j < K; j++) {
-            float d = (cluster[j].x - ponto[i].x) * (cluster[j].x - ponto[i].x) +
+            d = (cluster[j].x - ponto[i].x) * (cluster[j].x - ponto[i].x) +
                       (cluster[j].y - ponto[i].y) * (cluster[j].y - ponto[i].y);
             ponto[i].cluster = (d < distancia ? j : ponto[i].cluster);
             distancia = d < distancia ? d : distancia;
+
+            //printf("Analisys :T%d cluster (%f,%f), ponto (%f,%f)\n", id, cluster[j].x,
+            //cluster[j].y,ponto[i].x,ponto[i].y);
         }
     }
+
 }
 
 void mean() {
     float sum_x[K];
     float sum_y[K];
     int counter[K];
+    float aux_x,aux_y;
 
     for(int i = 0 ;  i < K  ; i++){
         sum_x[i] = 0.0;
         sum_y[i] = 0.0;    
         counter[i] = 0;
     }
-
-    #pragma omp parallel for reduction(+:sum_x,sum_y,counter) schedule(guided,N/3)
+    
+    #pragma omp parallel for schedule(static,N) reduction(+:sum_x[:K],sum_y[:K],counter[:K])
     for(int i = 0 ;  i < N ; i++){
         sum_x[ponto[i].cluster] += ponto[i].x;
         sum_y[ponto[i].cluster] += ponto[i].y;
         counter[ponto[i].cluster] ++;
     }
-
     for(int i = 0 ; i < K ; i++){
-        float aux_x = sum_x[i] / counter[i];
-        float aux_y = sum_y[i] / counter[i];
+        aux_x = sum_x[i] / counter[i];
+        aux_y = sum_y[i] / counter[i];
         if ((cluster[i].x- aux_x) != 0 || (cluster[i].y - aux_y) != 0) {
             cluster[i].x = aux_x;
             cluster[i].y = aux_y;
             cluster[i].cluster = counter[i];
         }
     }
+    
 }
 
 int main(int argc, char * argv[]) {
@@ -88,9 +95,9 @@ int main(int argc, char * argv[]) {
     ponto = (Ponto *) malloc(N * sizeof(Ponto));
     cluster = (Ponto *) malloc(K * sizeof(Ponto));
     inicializa();
+    omp_set_num_threads(T);
     colocar();
 
-    omp_set_num_threads(T);
     for(int i = 0 ; i < 20; i++){
         mean();
         colocar();
